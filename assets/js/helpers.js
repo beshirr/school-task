c0be6e45b79c1a12cec908fcc4cad2ecd1c1a5f7
beshirr <youssefahmedbeshir@gmail.com>
2025-04-20 17:11:56 +0200

    A pop up window for deletion confirmation in task list

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -5,3 +5,34 @@ function include(file) {
       document.body.insertAdjacentHTML("afterbegin", data);
     });
 }
+
+function show_delete_confirm_window(onConfirm) {
+  const delete_confirm = document.getElementById("confirmWindow");
+  const confirmBtn = document.getElementById("confirmBtn");
+  const cancelBtn = document.getElementById("cancelBtn");
+
+  delete_confirm.style.display = "flex";
+  document.body.classList.add("box-open");
+
+  confirmBtn.onclick = () => {
+    delete_confirm.style.display = "none";
+    document.body.classList.remove("box-open");
+    onConfirm();
+  };
+
+  cancelBtn.onclick = () => {
+    delete_confirm.style.display = "none";
+    document.body.classList.remove("box-open");
+  };
+}
+
+function confirm_delete() {
+  document.querySelectorAll('.delete-btn').forEach((btn) => {
+    btn.addEventListener('click', () => {
+      show_delete_confirm_window(() => {
+        // your delete logic here
+        console.log("Task deleted!");
+      });
+    });
+  });
+}
